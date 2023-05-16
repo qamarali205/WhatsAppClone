4415028cd24eb43aeff874fd7cf2ee91a33aceae
qamarali205 <qamarali205@gmail.com>
2023-05-16 11:59:57 +0530

    server

--- server/src/Controllers/chat.controller.js
+++ server/src/Controllers/chat.controller.js
@@ -1,7 +1,28 @@
+const Chat = require("../models/chat.model");
 
 
 const createChat=async (req,res)=>{
     try {
+        const {userId}=req.body;
+
+        const reqUser=await req.user;
+        if(!userId){
+            return res.status(400).json({error:"User Id require for creating chat"});
+        }
+
+        var isChat=await Chat.find({
+            isGroupChat:false,
+            $and:[
+                {users:{$eleMatch:{$eq:reqUser.user._id}}},
+                {users:{$eleMatch:{$eq:reqUser.userId}}},
+
+            ]
+        }).populate("users", "password").populate("latestmessage");
+
+        isChat=await User.populate(isChat,{
+            path:"latestMessage.sender",
+            select:"name profile_image email"
+        })
         
     } catch (error) {
         
