74d9fbb6ea839cbe9ab638fbd8aabd0891678b71
Qamar Ali <qamarali205@gmail.com>
2023-07-24 20:04:08 +0530

    Update Login.jsx

--- client/src/components/Registration/Login.jsx
+++ client/src/components/Registration/Login.jsx
@@ -98,13 +98,13 @@ const Login = () => {
             signup
           </p>
         </div>
-        <h3>For Login</h3>
+        <h3>For Login Demo Id Password</h3>
         <div className="flex space-x-3 item-center mt-2">
-          <p className="">email-</p>
+          <p className="">Email-</p>
           <p className="text-blue-500 hover:text-blue-800">admin@gmail.com</p>
         </div>
         <div className="flex space-x-3 item-center mt-2">
-          <p className="">password-</p>
+          <p className="">Password-</p>
           <p className="text-blue-500 hover:text-blue-800">admin</p>
         </div>
       </div>
